c2629d8893dabfc5891c127598600e1425e81e7c
fariddhessami <fariddhessami@gmail.com>
2020-07-17 17:12:08 +0430

    first edit mode tests

---
+++ Assets/editTests/Plant_Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Plant_Tests : MonoBehaviour
+{
+
+
+    [Test]
+    public void METHOD()
+    {
+        //    ACT
+        
+        //    ASSERT
+
+        Assert.AreEqual(1, 1);
+        Assert.AreEqual(1, 2);
+    }
+}
